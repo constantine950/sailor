9951ebdb2a7d630fd28de1e9cb576a931692e573
james <omotoyosiadebeshin@gmail.com>
2023-12-25 00:53:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing : {
+        '53': '13.5rem',
+        '62': '15.3rem',
+        '94': '22rem',
+      },
+    },
+  },
+  plugins: [],
+}
