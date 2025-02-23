7ee093efd23b9a8ffb6ad0db500af8b265420f1b
james <omotoyosiadebeshin@gmail.com>
2025-02-24 00:11:19 +0100

    Updated project with new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {
-      spacing : {
-        '53': '13.5rem',
-        '62': '15.3rem',
-        '98': '25rem',
+      spacing: {
+        53: "13.5rem",
+        62: "15.3rem",
+        98: "25rem",
       },
     },
   },
   plugins: [],
-}
+};
